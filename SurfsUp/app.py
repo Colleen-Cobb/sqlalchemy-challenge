356e54a6e5ca5463f9572fe14b7d883b881c107b
Colleen-Cobb <colleen.cobb5@gmail.com>
2022-11-19 13:11:59 -0500

    updating app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -9,8 +9,6 @@ from sqlalchemy import create_engine, func
 
 # 1. Import Flask
 from flask import Flask, jsonify
-
-
 #################################################
 # Database Setup
 #################################################
@@ -39,17 +37,18 @@ app = Flask(__name__)
 # Define what to do when a user hits the index route
 @app.route("/")
 def home():
-    print("Server received request for 'Home' page...")
     return (
-            f"Welcome to my 'Home' page! Here are the available routes:<br/>"
-            f"/api/v1.0/precipitation"
-            f"/api/v1.0/stations"
-            f"/api/v1.0/tobs"
-            f"/api/v1.0/<start>"
-            f"/api/v1.0/<start>/<end>"
-
+        f"Welcome to the climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )   
 
 # Define what to do when a user hits the /api/v1.0/precipitation route
+
 @app.route("/api/v1.0/precipitation")
 def prcp():
     #Create our Session (link) from Python to the DB
@@ -57,41 +56,76 @@ def prcp():
 
     """Return a list of precipitation data inlcuding date and prcp value for the last 12 months"""
 
-    # Query the prcp for the last 12 months
-    recent_date=session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
-    recent_day= (dt.datetime.strptime(recent_date, "%Y-%m-%d")).date()
-    year_ago= recent_day - dt.timedelta(days=365)
+    prcp_data = session.query(Measurement.date, Measurement.prcp).\
+                    filter(Measurement.date >= '2016-08-23').\
+                    filter(Measurement.date <= '2017-08-23').\
+                    order_by(Measurement.date).all()
 
-
-
-    results= session.query(Measurement.date, Measurement.prcp).\
-                filter((Measurement.date >= year_ago).all()
     
     session.close()
 
-    # Create a dictionary from the row data
-    prcp_values= []
-    for date, prcp in results:
-     
+    prcp_values= list(np.ravel(prcp_data))
 
-
-    print("Server received request for precipitation results...")
-    return "Here are the precipitation results from the last 12 months!"
     return jsonify(prcp_values)
 
 # Define what to do when a user hits the /api/v1.0/stations route
 @app.route("/api/v1.0/stations")
 def station():
-    print("Server received request for station results...")
-    return "Here are the stations located in Honolulu, Hawaii"
+   #Create our Session (link) from Python to the DB
+    session=Session(engine)
 
-# Define what to do when a user hits the /api/v1.0/tobs route
+    """Return a list of stations from the dataset"""
+
+    stations= session.query(Station.name).all()
+
+    session.close()
+
+    station_list=list(np.ravel(stations))
+
+    return jsonify(station_list)
+
+# # Define what to do when a user hits the /api/v1.0/tobs route
 @app.route("/api/v1.0/tobs")
 def tobs():
-    print("Server received request for temperature observations for the most active station...")
+       #Create our Session (link) from Python to the DB
+    session=Session(engine)
+
+    """Return a list of temperature observations for the most active station"""
+
+    sel=[Station.station, func.count(Measurement.station)]
+
+    active_station=session.query(*sel).\
+        filter(Station.station==Measurement.station).\
+        group_by(Station.station).\
+        order_by(func.count(Measurement.station).desc()).all()
+
+    most_active=session.query(*sel).\
+        filter(Station.station==Measurement.station).\
+        group_by(Station.station).\
+        order_by(func.count(Measurement.station).desc()).first()
+
+    most_active_station=most_active[0]
+
+
+    sel_temp = [(Measurement.date),
+        (Measurement.tobs)]
+
+    tobs= session.query(*sel_temp).\
+        filter(Measurement.station == most_active_station).\
+        filter(Measurement.date >= '2016-08-23').\
+        filter(Measurement.date <= '2017-08-23').all()
 
 
 
 
-if __name__ == "__main__":
+
+    session.close()
+
+    tobs_list=list(np.ravel(tobs))
+
+    return jsonify(tobs_list)
+
+
+
+if __name__ == "__main__" : 
     app.run(debug=True)
